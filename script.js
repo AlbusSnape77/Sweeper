2b1d2d00dc4a93abfef5eb0086bedea5dd1e9110
AlbusSnape <AlbusSnape77@gmail.com>
2023-12-23 22:28:39 +0800

    实现空白展开，右键标记

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ function renderMineField(){
             let cellEl = document.createElement('div');
             cellEl.className = "cell";
 
+            cellEl.onclick = function() {
+                
+                handleClick(i, j, cells, m, n);
+            };  
+
+            cellEl.oncontextmenu = function(event) {
+                console.log("contextmenu",i,j);
+                handleFlagging(i, j, cells, m, n);
+                // event.stopPropagation();
+                // event.preventDefault();
+            };
+
             tdEl.append(cellEl);
             row.push({
                 mined: false,
@@ -48,6 +60,8 @@ function renderMineField(){
     checkAmbeientMinedCounts(cells, m, n);
 }
 
+
+
 let directions = [
     [-1,-1],[0,-1],[1,-1],
     [-1,0],[1,0],
@@ -55,36 +69,36 @@ let directions = [
 ]
 
 function checkAmbeientMinedCounts(cells, m, n) {
-    for (let i = 0; i < m; i++){
-        for (let j = 0; j < n; j++){
-            let cell = cells[i][j];
+    for (let rowIdx = 0; rowIdx < m; rowIdx++){
+        for (let colIdx = 0; colIdx < n; colIdx++){
+            let cell = cells[rowIdx][colIdx];
             if (cell.mined) {
                 continue;
             }
 
-            let minedCount = 0;
-            for (let [dx,dy] of directions) {
-                let newRowIdx = i + dx, newColIdx = j + dy;
+            let mineCount = 0;
+            for (let [drow,dcol] of directions) {
+                let newRowIdx = rowIdx + drow, newColIdx = colIdx + dcol;
                 if (newRowIdx < 0 || newRowIdx >= m || 
                     newColIdx < 0 || newColIdx >= n) {
                     continue;
                 }
                 if (cells[newRowIdx][newColIdx].mined) {
-                    minedCount += 1;
+                    mineCount += 1;
                 }                
             }
 
-            if (minedCount > 0) {
+            if (mineCount > 0) {
                 let countSpan = document.createElement("span");
                 countSpan.className = "mine-count";
-                countSpan.innerText = `${minedCount}`;
+                countSpan.innerText = `${mineCount}`;
                 
-                countSpan.classList.add(`n${minedCount}`);
+                countSpan.classList.add(`n${mineCount}`)
 
                 cell.el.append(countSpan);
 
             } 
-             console.log(i, j, minedCount);
+            cell.mineCount = mineCount;
         }
     }
 
@@ -105,6 +119,82 @@ function randomMineFieldNo(m, n, mineNums) {
     }
 
     return mines;
+}
+
+function handleClick(rowIdx,colIdx, cells, m, n) {
+    let cell = cells[rowIdx][colIdx]
+    if(cell.mined) {
+
+    } else if (cell.mineCount == 0) {
+        spreadSafeField(rowIdx,colIdx, cells, m, n)
+
+    } else if (cell.mineCount > 0) {
+
+    }
+    console.log(cells[rowIdx][colIdx]);
+}
+
+function handleFlagging(rowIdx, colIdx, cells, m, n) {
+    let cell = cells[rowIdx][colIdx];
+    if (cell.spreaded) {
+        return;
+    }
+
+    console.log(rowIdx, colIdx, cell);
+    setFlag(cell, !cell.flag);
+}
+
+function setFlag(cell, flag) {
+    if (flag) {
+        cell.flag = true;
+        cell.el.innerHTML =`<span class="flag">!</span>`;
+    } else {
+        cell.flag = false;
+        if (cell.mined) {
+            cell.el.innerHTML =`<span class="mine">*</span>`;
+        } else if (cell.mineCount > 0) { 
+            cell.el.innerHTML =`<span class="mine-count n${cell.mineCount}">${cell.mineCount}</span>`;
+        } else {
+            cell.el.innerHTML = "";
+        }
+    }
+}
+
+function spreadSafeField(rowIdx,colIdx, cells, m, n) {
+    let cell = cells[rowIdx][colIdx];
+    if (!cell.spreaded){
+        cell.spreaded = true;
+        cell.el.classList.add("spreaded");
+        if  (cell.flag) {
+            setFlag(cell, false);
+        }
+    }
+
+    for (let [drow,dcol] of directions) {
+            let newRowIdx = rowIdx + drow, newColIdx = colIdx + dcol;
+            if (newRowIdx < 0 || newRowIdx >= m || 
+                newColIdx < 0 || newColIdx >= n) {
+                continue;
+            }
+        let cell = cells[newRowIdx][newColIdx];
+        if (cell.spreaded) {
+            continue;
+        }
+        if(!cell.spreaded && cell.mineCount == 0){
+            spreadSafeField(newRowIdx,newColIdx, cells, m, n); 
+        }
+
+        cell.spreaded = true;
+        cell.el.classList.add("spreaded");
+
+        if  (cell.flag) {
+            console.log("rc:", rowIdx, colIdx);
+            setFlag(cell, false);
+        }
+
+
+        
+    }
 }
 
 
